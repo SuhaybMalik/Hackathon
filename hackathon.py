fcd480831898d33ecb8308017b14ff06296c8b97
Maxwell D <145219537+Maxwellwellium@users.noreply.github.com>
2024-02-24 16:34:38 -0500

    fixed attribute error; added accepting requests

--- hackathon.py
+++ hackathon.py
@@ -2,35 +2,76 @@ import datetime
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="deez")
 
-class Trip:
+class Users:
     def __init__(self):
-        self.timeStart = datetime.datetime.now()
-        self.pool = []
-
-    def __repr__(self):
-        return str([self.timeStart.strftime("%Y-%m-%d %H:%M:%S"), self.pool])
+        self.userList = []
     
-    def addperson(self):
-        destination = input("enter destination")
-        username = input("enter name")
-        newPerson = Person(destination, username)
-        self.pool.append(newPerson.__repr__())
-        newPath = newPerson.__repr__()
-        #print(newPath)
+    def addUser(self, newUser):
+        if self.userList.length == 0:
+            self.userList.insert(newUser)
+        else:
+            for i, existingUser in enumerate(self.userList):
+                if newUser.upper < existingUser.upper():
+                    self.userList.insert(i, newUser)
+                    break
+            
+class Trip:
+    def __init__(self, driver, startLocation, destination, startTime):
+        self.startTime = startTime
+        self.rideRequests = []
+        self.riders = []
+        self.driver = driver
+        self.startLocation = startLocation
+        self.destination = destination
+        
+    def __repr__(self):
+        return str([self.startTime.strftime("%Y-%m-%d %H:%M:%S"), self.driver , self.riders])
+    
+    def requestRide(self, riderinfo):
+        self.rideRequests.append(riderinfo)
+    
+    def getStartTime(self):
+        return self.startTime
+    
+    def addRider(self, rider):
+        #self.rideRequests.pop(rider)
+        self.riders.append(rider)
 
-class Person:
-    def __init__(self, endpoint, alias):
-        self.alias = alias
-        self.endpoint = endpoint
-        self.path = geolocator.geocode(self.endpoint)
+class Account:
+    def __init__(self, username):
+        self.username = username
+        self.tripHistory = []
+        self.plannedTrips = []
+        self.path = 'placeholder'
 
     def __repr__(self):
-        return [str(self.path), self.alias]
+        return str([self.path, self.username])
+    
+    def newDriverTrip(self):
+        startLocation = geolocator.geocode(input("Enter Start Location: "))
+        destination = geolocator.geocode(input("Enter Driver Destination: "))
+        startTime = datetime.datetime.now()
+        thisTrip = Trip(self, startLocation, destination, startTime)
+        self.plannedTrips.append(thisTrip)
+        #startTime = input("Enter Start Time: ") 
+        return thisTrip
 
-newTrip = Trip()
-newTrip.addperson()
-newTrip.addperson()
-newTrip.addperson()
+    def riderTripRequest(self, trip):
+        destination = geolocator.geocode(input("Enter Destination: "))
+        trip.requestRide((self.username , destination))
 
-newtime = newTrip
-print(newtime)
+    def acceptRequest(self, trip, rider):
+        trip.addRider(rider)
+        
+driver = Account("driver")
+rider1 = Account("rider1")
+rider2 = Account("rider2")
+
+trip = driver.newDriverTrip()
+
+rider1.riderTripRequest(trip)
+rider2.riderTripRequest(trip)
+
+driver.acceptRequest(trip, rider2)
+
+print(trip)
